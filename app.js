da8119fde0c0f921731a4c2d6116beae6b2010d3
Dat Nguyen <nguyenquocdat2511998@gmail.com>
2020-06-24 22:27:21 +0700

    fix app.js

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 
 app.use(express.json());
 
